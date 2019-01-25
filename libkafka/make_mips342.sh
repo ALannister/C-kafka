4e6de4d56939649a0a60a56d38c4159c043c2f58
Lannister <1579624684@qq.com>
2019-01-25 11:05:04 +0800

    创建 librdkafka

---
+++ libkafka/make_mips342.sh
@@ -0,0 +1,5 @@
+
+export	WORKROOT=${PWD}
+export	CONFIG_FILE=${WORKROOT}/make/config_mips342_linux
+export  OUTPUT_DIR=${WORKROOT}/rdkafka/mips/gcc342
+make -f Makefile
---
+++ libkafka/make_mips434.sh
@@ -0,0 +1,5 @@
+
+export	WORKROOT=${PWD}
+export	CONFIG_FILE=${WORKROOT}/make/config_mips434_linux
+export  OUTPUT_DIR=${WORKROOT}/rdkafka/mips/gcc434
+make -f Makefile
